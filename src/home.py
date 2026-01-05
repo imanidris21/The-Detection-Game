c7dfc7e726d3248f91623749be638eabf6481746
ImanAhmed <imanidris21@gmail.com>
2026-01-05 16:17:01 +0000

    ui changes after the festival

--- src/home.py
+++ src/home.py
@@ -19,10 +19,10 @@ def get_base64_of_image(image_path):
 init_db()
 
 # Main content: title and subheading
-st.markdown("<h1 style='text-align: center; font-size: 7rem; margin-top: 4rem;'>Human or Machine?</h1>", unsafe_allow_html=True)
+st.markdown("<h1 style='text-align: center; font-size: 4rem; margin-top: 4rem;'>Human or Machine?</h1>", unsafe_allow_html=True)
 
 st.markdown("""
-<div style='text-align: center; max-width: 1000px; margin: 0 auto; padding: 0 2rem;'>
+<div style='text-align: center; max-width: 1000px; margin: 0 auto; padding: 0 1rem;'>
 <h3 style='font-size: 3rem;'>Do you think you can spot the AI-generated artworks and beat our AI Detector</h3>
 
 </div>
